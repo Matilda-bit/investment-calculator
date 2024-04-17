8de72a38261c0387eb1730d4ebcbf8acb1e88e3f
Matilda-bit <polinaovras@gmail.com>
2024-04-17 18:52:58 +0300

    added user area to input

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import UserInput from "./components/UserInput";
 
 function App() {
   return (
     <>
       <Header />
+      <UserInput />
      <h1>React Investment Calculator</h1>
     </>
    
